e8ec606bcbb3952932d78f3c1127366541c365e1
Gabriel Vazarini <camaravazarini@gmail.com>
2022-05-27 00:41:24 -0300

    Vetores

---
+++ Vet_Produtos/Produto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vet_Produtos
+{
+    internal class Produto
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Vet_Produtos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vet_Produtos
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Produto[] vet = new Produto[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vet[i] = new Produto { Name = name, Price = price };
+            }
+
+            double sum = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += vet[i].Price;
+            }
+            double avg = sum/ n;
+            Console.WriteLine("O preco medio dos produtos e de: $ " + avg.ToString("F2", CultureInfo.InvariantCulture)); 
+        }
+
+    }
+}
